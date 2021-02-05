951e179b5d87ea463b308bef68713c86fb71efdd
Key Mitchell <keygunmitchell@gmail.com>
2021-02-05 04:15:10 +0000

    render pyramid to the DOM instead of the console

--- mario.js
+++ mario.js
@@ -1,7 +1,7 @@
 /*
  * printPyramid
  *
- * Prints to the console a pyramid of '#' characters of the specified height
+ * Prints a pyramid of '#' characters of the specified height
  * For example, if height is 5, the console will look like this:
  *          ##
  *         ###
@@ -11,18 +11,30 @@
  */
 
 //Function to pass user inputted height and build a mario pyramid
+function printPyramid() {
+	let height = document.getElementById("height").value;
+	let output = "";
+	let rowstr = "";
+	for (let rows = 1; rows <= height; rows++) {								//outer loop starts
+		for (let space = height - 1; space >= rows; space--) {		//inner loop adds spaces
+			output += ".";
+			rowstr += ".";
+		}
+		for (let brick = 0; brick <= rows; brick++) {					//inner loop adds bricks
+			output += "#";
+			rowstr += "#";
+		}
+		output += "\n";		//start next layer
 
-document.getElementById("btn").addEventListener("click", function printPyramid() {
-	var height = document.getElementById('height').value;
-    let output = "";
-    for(let rows = 1; rows <= height; rows++) {								//outer loop starts
-        for(let space = height-1; space >= rows; space--) {		//inner loop adds spaces
-               output += " ";
-        }
-        for(let brick = 0; brick <= rows; brick++) {					//inner loop adds bricks
-            output += "#";
-        }
-        output += "\n";		//start next layer
-    }											//outer loop stops
-    console.log(output);	//print pyramid
-}  );
+		//adds pyramid to pyramid div
+		let pyra = document.createElement("p");
+		let mid = document.createTextNode(rowstr);
+		pyra.appendChild(mid);
+
+		let element = document.getElementById("pyramid");
+		element.appendChild(pyra);
+
+		rowstr = "";
+	}											//outer loop stops
+	console.log(output);	//print pyramid
+}  
